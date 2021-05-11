33265ae498fe231dc722720028c859d8ce17d839
Svetlana Tchistiakova <svetlana@vor.inf.ed.ac.uk>
2021-05-11 13:26:21 +0000

    feat(utils): add get_line_offsets and setup_logger to utils

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import logging
 import os
 import shutil
 import subprocess
+import sys
 import textwrap
 from typing import *
 from typing import IO #above * doesn't get this one
@@ -85,3 +86,50 @@ def write_json_line(data: Union[dict, list], fh: IO):
     r"""Write json-compatible data into the fh stream."""
     s = json.dumps(data, ensure_ascii=False, sort_keys=True)
     fh.write(s + '\n')
+
+def get_line_offsets(fh):
+    """
+    Create a dict of line numbers to their byte offset. After this, you can 
+    read a arbitrary line i with: file.seek(line_offsets[i]); file.readline()
+    """
+    line_offsets = {0: 0} 
+    for i, line in enumerate(fh):
+        if line != '': #this is EOF
+            line_offsets[i+1] = fh.tell()
+    fh.seek(0)
+    return line_offsets
+
+def setup_logger(
+        name='scripts', 
+        folder=None, 
+        level=logging.DEBUG, 
+        to_stdout=True
+    ):
+    r"""Write logs to the file at folder/name.log. Default folder is cwd."""
+    if folder is None:
+        folder = os.getcwd()
+    os.makedirs(folder, exist_ok=True)
+    fp = os.path.join(folder, name + '.log')
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    #delay is a half-fix for mem leak: https://bugs.python.org/issue23010
+    file_handler = logging.FileHandler(fp, delay=True) 
+    file_handler.setLevel(logging.DEBUG)
+    file_formatter = logging.Formatter('[%(asctime)s:%(levelname)s:%(name)s:%(lineno)d] %(message)s')
+    file_handler.setFormatter(file_formatter)
+
+    stream_handler = None
+    if to_stdout:
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setLevel(logging.INFO)
+        stream_formatter = logging.Formatter('[%(levelname)s:%(name)s:%(lineno)d] %(message)s')
+        stream_handler.setFormatter(stream_formatter)
+
+    if not logger.handlers:
+        logger.addHandler(file_handler)
+        if to_stdout:
+            logger.addHandler(stream_handler)
+
+    return logger
