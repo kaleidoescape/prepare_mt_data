d9b379007996ad8cf3979e862a9f246fa97d169d
Svetlana Tchistiakova <stchistiakova@gmail.com>
2021-05-28 09:52:54 +0000

    feat(resample): add options for resamping w/ temperature, uniform, original; rename script

--- resample_to_mono.py
+++ resample_to_mono.py
@@ -19,26 +19,41 @@ logger.setLevel(logging.INFO)
 def temperature_sampling(sizes, temperature=5):
     sizes = np.asarray(sizes)
     distrib = sizes ** (1 / temperature)
-    distrib = distrib / sum(distrib)
-    return distrib
+    return distrib / sum(distrib)
+
+def original_distrib_sampling(sizes):
+    sizes = np.asarray(sizes)
+    return sizes / sum(sizes)
+
+def uniform_sampling(sizes):
+    return np.asarray([1/len(sizes) for i in sizes])
 
 def main(
         pconfig: str,
         outdir: str,
         n: int,
+        sampling_method: str, 
         temperature: Optional[float]=5,
+        dataset_sampling_method: str, 
         dataset_temperature: Optional[float]=5,
     ):
     r"""
     Resample multilingual or monolingual training data into new 
     MONOLINGUAL files using a particular sampling distribution. 
 
+    NOTE: We sample from datasets line-by-line, and do not pre-shuffle
+    the dataset, so if this is necessary, please pre-shuffle it first.
+
     Args:
         config: config file with test sets
         outdir: output directory to put intermediate files into
         n: number of sentences to sample into the new output files
-        temperature: T parameter for sampling from a lang
-        dataset_temperature: T parameter for sampling from a lang-specific dataset
+        sampling_method: {temperature,uniform,orig}
+        temperature: T parameter for sampling from a lang (only used when
+            samping_method==temperature)
+        dataset_sampling_method: {temperature,uniform,orig}
+        dataset_temperature: T parameter for sampling from a lang-specific 
+            dataset (only used when dataset_sampling_method==temperature)
 
     Example config:
         { data: { 
@@ -84,15 +99,30 @@ def main(
         lengths[lang] = [utils.get_file_length(fp) for fp in langs[lang]]
         for i, fp in enumerate(langs[lang]):
             dataset_lengths[fp] = lengths[lang][i]
-        dataset_distribs[lang] = temperature_sampling(lengths[lang], dataset_temperature)
+
+        #create the sampling distribution for the datasets of this lang
+        if dataset_sampling_method == 'temperature':
+            dataset_distribs[lang] = temperature_sampling(lengths[lang], dataset_temperature)
+        elif dataset_sampling_method == 'uniform':
+            dataset_distribs[lang] = uniform_sampling(lengths[lang])
+        elif dataset_sampling_method == 'orig':
+            dataset_distribs[lang] = original_distrib_sampling(lengths[lang])
+
         for fp in langs[lang]:
             dataset_fhs[fp] = open(fp, 'r', encoding='utf-8')
         logger.info(f"Available datapoints for '{lang}' across {len(lengths[lang])} datasets: {sum(lengths[lang])}")
 
-    #temperature sample from languages
     lang_lengths = [sum(lengths[l]) for l in sorted_langs]
+
+    #create the sampling distribution over the languages
+    if sampling_method == 'temperature':
+        distrib = temperature_sampling(lang_lengths, temperature)
+    elif sampling_method == 'uniform':
+        distrib = uniform_sampling(lang_lengths)
+    elif sampling_method == 'orig':
+        distrib = original_distrib_sampling(lang_lengths)
+
     total_datapoints = sum(lang_lengths)
-    distrib = temperature_sampling(lang_lengths, temperature)
     logger.info(f"Sampling {n} samples from total length {total_datapoints}")
     logger.info(f"Resampled distribution for {sorted_langs}: {distrib}")
     sampled_lang_idxs = np.argmax(np.random.multinomial(1, distrib, n), axis=1)
@@ -156,10 +186,14 @@ def parse_args():
         help="output directory where to save results")
     parser.add_argument('--n', required=True, type=int,
         help="the number of new sentences to sample")
+    parser.add_argument('--sampling-method', required=True, choices=['temperature', 'uniform', 'orig'],
+        help="the sampling method to use for sampling from the language distribution")
     parser.add_argument('--temperature', default=5.0, type=float,
-        help="temperature sampling parameter T for sampling a language; new distribution will be (sizes ** (1/T) / sum(sizes ** (1/T))")
+        help="only applicable in case sampling-method==temperature; temperature sampling parameter T for sampling a language; new distribution will be (sizes ** (1/T) / sum(sizes ** (1/T))")
+    parser.add_argument('--dataset-sampling-method', default='orig', choices=['temperature', 'uniform', 'orig'],
+        help="the sampling method to use for sampling from each dataset within a language")
     parser.add_argument('--dataset_temperature', default=5.0, type=float,
-        help="temperature sampling parameter T for sampling a datasets under a given language")
+        help="only applicable in case dataset-sampling-method==temperature; temperature sampling parameter T for sampling a datasets under a given language")
     args, rest = parser.parse_known_args()
     args.rest = rest 
     
